d2b4c29eaa8e6b595d6d06df8ccd101345cc529c
Andriy Panasiuk <panasiukmail@gmail.com>
2023-03-01 14:23:59 +0200

    feat: header, footer, routes, components pathes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
-import { useState } from 'react'
 import './App.scss'
+import { Routes, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+
+import { Navbar, Homepage, Cryptocurrencies, CryptoDetails, News, Exchanges } from './components'
 
 function App() {
 
   return (
     <div className="App">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />}>
+              </Route>
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />}>
+              </Route>
+              <Route path="/crypto/:coinId" element={<CryptoDetails />}>
+              </Route>
+              <Route path="/exchanges" element={<Exchanges />}>
+              </Route>
+              <Route path="/news" element={<News />}>
+              </Route>
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+            <Link to="/">
+              Cryptoverse Inc.
+            </Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from '../components/Navbar/Navbar.jsx';
+export { default as Cryptocurrencies } from '../components/Cryptocurrencies/Cryptocurrencies.jsx';
+export { default as CryptoDetails } from '../components/CryptoDetails/CryptoDetails.jsx';
+export { default as Homepage } from '../components/Homepage/Homepage.jsx';
+export { default as News } from '../components/News/News.jsx';
+export { default as Exchanges } from '../components/Exchanges/Exchanges.jsx';
